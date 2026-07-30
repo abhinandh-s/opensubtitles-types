fe3fbbc300dcb2ffb03f7859e9bbcc155696a1ae
Abhinandh S <abhinandhsuby@proton.me>
2026-07-30 14:30:50 +0530

    Update download.rs

--- src/download.rs
+++ src/download.rs
@@ -4,23 +4,17 @@ pub const DOWNLOAD_URL: &str = "https://api.opensubtitles.com/api/v1/download";
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct DownloadRequest {
-    pub file_id: u64, // Required
-    
+    pub file_id: u64, // Required    
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub sub_format: Option<String>,
-    
+    pub sub_format: Option<String>,    
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub file_name: Option<String>,
-    
+    pub file_name: Option<String>,    
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub in_fps: Option<f64>,
-    
+    pub in_fps: Option<f64>,    
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub out_fps: Option<f64>,
-    
+    pub out_fps: Option<f64>,    
     #[serde(skip_serializing_if = "Option::is_none")]
     pub timeshift: Option<f64>,
-
     #[serde(skip_serializing_if = "Option::is_none")]
     pub force_download: Option<bool>,
 }
