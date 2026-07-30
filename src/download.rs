70364070333b6e31317970d41e0d078ddfc4ebed
Abhinandh S <abhinandhsuby@proton.me>
2026-07-30 13:49:03 +0530

    Update download.rs

--- src/download.rs
+++ src/download.rs
@@ -1,7 +1,30 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Download {
+pub struct DownloadRequest {
+    pub file_id: u64, // Required
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sub_format: Option<String>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub file_name: Option<String>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub in_fps: Option<f64>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub out_fps: Option<f64>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timeshift: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub force_download: Option<bool>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct DownloadResponse {
     pub link: String,
     pub file_name: String,
     pub requests: u64,
